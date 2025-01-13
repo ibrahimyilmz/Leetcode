ad32d5bbfdc1224c7fd2043b84f49809c3066632
İbrahim Enes Yılmaz <145198928+ibrahimyilmz@users.noreply.github.com>
2025-01-13 11:40:30 +0300

    Time: 11 ms (77.02%) | Memory: 32 MB (86.69%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.cpp
@@ -2,21 +2,15 @@ class Solution {
 public:
     int minimumLength(string s) {
         vector<int> freq(26, 0);
-        int length = 0;
+
         for (char ch : s) {
             freq[ch - 'a']++;
-            length++;
         }
 
+        int length = 0;
         for (int i = 0 ; i < 26 ; i++) {
-            if (freq[i] > 2) {
-                length -= freq[i];
-                while (freq[i] > 2) {
-                    freq[i] -= 2;
-                }
-                length += freq[i];
-            }
-
+            if (freq[i] % 2 == 1) length += 1;
+            else if (freq[i] % 2 == 0 && freq[i] != 0) length += 2;
         }
         return length;
     }
