9b61e92b380185a00ebe544dadaa8c92a741de5b
İbrahim Enes Yılmaz <145198928+ibrahimyilmz@users.noreply.github.com>
2025-01-28 18:01:36 +0300

    Time: 0 ms (100.00%) | Memory: 12.7 MB (78.43%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -10,36 +10,22 @@
  * };
  */
 class Solution {
+private:
+    bool helper(TreeNode* p, TreeNode* q) {
+        if (!p && !q) return true;
+        if (!(p && q)) return false;
+        if (p->val != q->val) return false;
+
+        bool l = helper(p->left, q->left);
+        bool r = helper(p->right, q->right);
+
+        return l && r;
+    }
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if (!p && !q) return true;
         if (!(p && q)) return false;
 
-        queue<TreeNode*> queP;
-        queP.push(p);
-        queue<TreeNode*> queQ;
-        queQ.push(q);
-
-        while(!queP.empty() && !queQ.empty()) {
-            p = queP.front();
-            queP.pop();
-            q = queQ.front();
-            queQ.pop();
-
-            if (p->val != q->val) return false;
-
-            if (p->right && !q->right || !p->right && q->right) return false;
-            if (p->left && !q->left || !p->left && q->left) return false;
-
-            if (p->right) queP.push(p->right);
-            if (p->left) queP.push(p->left);
-
-            if (q->right) queQ.push(q->right);
-            if (q->left) queQ.push(q->left); 
-        }
-
-        if (queP.empty() && queQ.empty()) return true;
-
-        return false;
+        return helper(p, q);
     }
 };
