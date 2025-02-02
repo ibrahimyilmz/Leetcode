429248a3d6c9953a45aa62be17497b0e6390d88e
İbrahim Enes Yılmaz <145198928+ibrahimyilmz@users.noreply.github.com>
2025-02-02 10:33:50 +0300

    Time: 0 ms (100.00%) | Memory: 24.3 MB (67.81%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -11,25 +11,19 @@
  */
 class Solution {
 private:
-    int help(TreeNode* root, int& k) {
-        if (!root) {
-            k++;
-            return -1;
-        }
+    int binaryTurbo(TreeNode* root, int& k) {
+        if (!root) return -1;
 
-        int l = help(root->left, k);
+        int l = binaryTurbo(root->left, k);
         k--;
         if (k == 0) return root->val;
-        k--;
-        if (k == 0) return root->val;
-        int r = help(root->right, k);
+        int r = binaryTurbo(root->right, k);
 
         if (l != -1) return l;
-        if (r != -1) return r;
-        return -1;
+        return r;
     }
 public:
     int kthSmallest(TreeNode* root, int k) {
-        return help(root, k);
+        return binaryTurbo(root, k);
     }
 };
